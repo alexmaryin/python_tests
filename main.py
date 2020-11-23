45914e6d3d121822c00a1bda92f00d9938bb5c78
java73 <java73@yandex.ru>
2020-11-24 00:03:49 +0400

    Quick sort and counts of numbers

--- main.py
+++ main.py
@@ -16,15 +16,32 @@ def quicksort(arr):
     return quicksort(less) + equals + quicksort(greats)
 
 
+def segments_count(arr, dim):
+    assert len(arr) > 0
+    result = [0] * dim
+    last = arr[0]
+    count = 0
+    for x in arr:
+        if x != last:
+            result[last - 1] = count
+            last = x
+            count = 0
+        count += 1
+    return result
+
+
 def main():
-    arr_dimension = 100000
-    arr_range = 100000
-    loops = 10
+    arr_dimension = 100
+    arr_range = 10
+    loops = 1
 
     start = millis()
     for loop in range(loops):
         arr = [random.randint(1, arr_range) for _ in range(arr_dimension)]
+        print(arr)
         arr_sort = quicksort(arr)
+        print(arr_sort)
+        print(segments_count(arr_sort, arr_range))
     print("Время работы быстрой сортировки {} раз массива из {} чисел в диапазоне {} - {} мс".format(loops,
           arr_dimension, arr_range, millis() - start))
 
