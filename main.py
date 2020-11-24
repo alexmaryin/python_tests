2884c2a97c2dd450a009852f5d7a5b3819eb5403
java73 <java73@yandex.ru>
2020-11-24 17:52:23 +0400

    даже не знаю что тут написать

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from collections import Counter
 #import time as time_
 
 
@@ -26,53 +27,20 @@ def quicksort(arr):
     return quicksort(less) + equals + quicksort(greats)
 
 
-def segments_count(arr):
-    assert len(arr) > 0
-    result = [0] * arr[len(arr) - 1]
-    last = arr[0]
-    count = 0
-    for x in range(len(arr)):
-    	if arr[x] != last:
-    		result[last - 1] = count
-    		count = 0
-    		last = arr[x]
-    	count += 1
-    result[len(result) - 1] = count if arr[len(arr) - 1] == last else 1
-    return result
-
-
 def main():
-	segments_count, points = map(int, input().split())
-	segments = [[int(x) for x in input().split()] for _ in range(segments_count)]
-	points = [int(x) for x in input().split()]
+	count, points = map(int, input().split())
+	segments = [[int(x) for x in input().split()] for _ in range(count)]
+	points = {int(x) for x in input().split()}
 	arr = []
 	for seg in segments:
 		for x in range(seg[0], seg[1] + 1):
 			arr.append(x)
 	s = quicksort(arr)
-	c = segments_count(s)
+	print(s)
+	c = Counter(s)
 	print(c)
-	res = [counted_arr[x] for x in points]
+	res = [c[x] for x in points]
 	print(*res)
-
-
-
-    #arr_dimension = 1000
-    #arr_range = 1000
-    #loops = 1
-
-    #start = millis()
-    #for loop in range(loops):
-        #arr = [random.randint(1, arr_range) for _ in range(arr_dimension)]
-        #print(arr)
-        #arr_sort = quicksort(arr)
-        #print(arr_sort)
-        #print("Время работы быстрой сортировки {} раз массива из {} чисел в диапазоне {} - {} мс".format(loops,
-        #  arr_dimension, arr_range, millis() - start))
-        #start = millis()
-        #print(segments_count(arr_sort, arr_range))
-        #print("Время подсчета количества каждого числа в массиве {} мс".format(millis() - start))
-    
 
 
 if __name__ == '__main__':
