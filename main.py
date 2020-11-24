10b9c5bc956d30eba1939d73f42bbc913edb7e35
alexmaryin <java73@yandex.ru>
2020-11-24 20:58:32 +0400

    Готовый алгоритм для решения задачи поиска количества вхождений точек на прямой в различные отрезки на этой прямой. Работает так: координаты начала и конца отрезков сразу кладутся в разные массивы, которые потом сортируются методом быстрой сортировки (3), т.е. с разбиением на три подмассива - меньшие, большие и равные числа. После путем бинарного поиска для каждой точки считается разность между первым и вторым массивом (для первого ищется место вставки максимально справа, т.е. >=, для второго - максимально слева, т.е. <).

--- main.py
+++ main.py
@@ -6,39 +6,29 @@ def quicksort(arr):
     if len(arr) <= 1:
         return arr
     pivot = random.choice(arr)
-    less = []
-    greats = []
-    equals = []
     less = [x for x in arr if x < pivot]
     greats = [x for x in arr if x > pivot]
     equals = [pivot] * arr.count(pivot)
-    #for x in arr:
-    #	if x < pivot:
-    #		less.append(x)
-    #	elif x > pivot:
-    #		greats.append(x)
-    #	else:
-    #		equals.append(x)
     return quicksort(less) + equals + quicksort(greats)
 
 
 def main():
-	count, points = map(int, input().split())
-	segL = []
-	segR = []
-	for x in range(count):
-		left, right = map(int, input().split())
-		segL.append(left)
-		segR.append(right)
-	points = [int(x) for x in input().split()]
-	segL = quicksort(segL)
-	segR = quicksort(segR)
-	results = []
-	for x in points:
-		left = bisect.bisect(segL, x) + 1
-		right = bisect.bisect(segR, x) + 1
-		results.append(left - right)
-	print(*results)
+    count, points = map(int, input().split())
+    segL = []
+    segR = []
+    for x in range(count):
+        left, right = map(int, input().split())
+        segL.append(left)
+        segR.append(right)
+    points = [int(x) for x in input().split()]
+    segL = quicksort(segL)
+    segR = quicksort(segR)
+    results = []
+    for x in points:
+        left = bisect.bisect(segL, x) + 1
+        right = bisect.bisect_left(segR, x) + 1
+        results.append(left - right)
+    print(*results)
 
 
 if __name__ == '__main__':
