00c075c129f5261f2527d789794be41c2cffc970
java73 <java73@yandex.ru>
2020-11-26 16:07:58 +0400

    добавлен алгоритм поиска самой длинной невозрастающей подпоследовательности

--- lis_find.py
+++ lis_find.py
@@ -1,17 +1,84 @@
+import random
+from sys import stdin
+from math import ceil
+
+
 def lis_bottom_up(arr):
     temp = [1] * len(arr)
     for i in range(len(arr)):
         for j in range(0, i):
-            if arr[j] < arr[i]:
+            #if arr[j] < arr[i]:						# для поиска строго возрастающей последовательности
+            # if arr[i] % arr [j] == 0:				# для поиска наибольшей последовательнократной подпоследовательности
+            if arr[j] >= arr[i]:		# для поиска наибольшей невозрастающей последовательности, т.е. 5 4 4 4 3 3 2 1 к примеру
                 temp[i] = max(temp[i], temp[j] + 1)
     ans = max(temp)
     return ans
 
 
+def lis_naive(arr):
+	lens = [1] * len(arr)
+	tops = [-1] * len(arr)
+	for i in range(len(arr)):
+		for j in range(0, i):
+			if arr[j] >= arr[i] and lens[i] < lens[j] + 1:
+				lens[i] = lens[j] + 1
+				tops[i] = j
+	ans_index = lens.index(max(lens))
+	path = []
+	path_indicies = []
+	while ans_index >= 0:
+		path.append(arr[ans_index])
+		path_indicies.append(ans_index + 1)
+		ans_index = tops[ans_index]
+	path.reverse()
+	path_indicies.reverse()
+	return path, path_indicies
+
+
+def lis_bisect(arr):
+	n = len(arr)
+	pred = [0] * n
+	indicies = [0] * (n + 1)
+	last = 0
+	arr = arr[::-1]
+	for i in range(n):
+		lo, hi = 1, last
+		while lo <= hi:
+			mid = ceil((lo + hi) / 2)
+			if arr[indicies[mid]] <= arr[i]:
+				lo = mid + 1
+			else:
+				hi = mid - 1
+		newlast = lo
+		pred[i] = indicies[newlast - 1]
+		if newlast > last:
+			indicies[newlast] = i
+			last = newlast
+		elif arr[i] < arr[indicies[newlast]]:
+			indicies[newlast] = i
+	seq = [0] * last
+	key = indicies[last]
+	for i in range(last - 1, -1, -1):
+		seq[i] = n - key
+		key = pred[key]
+	return seq
+
+
 def main():
-    inp = [1, 5, 3, 3, 5, 7, 8, 4, 9, 3, 3, 10]
-    print(lis_bottom_up(inp))
+	#max_len = 20
+	# inp = [random.randint(0, max_len ** 2) for _ in range(max_len)]
+	#inp = [5, 3, 4, 4, 2]
+	#print(inp)
+	#max_len = int(input())
+	#inp = [int(x) for x in input().split()]
+	#print(lis_bottom_up(inp))
+	# path, indicies = map(list, lis_naive(inp))
+	max_len = (int(stdin.readline()))
+	inp = [int(x) for x in stdin.readline().split()]
+	indicies = lis_bisect(inp)
+	print(len(indicies))
+	print(' '.join(map(str, indicies[::-1])))
 
 
 if __name__ == '__main__':
-    main()
+	main()
--- rank_sort.py
+++ rank_sort.py
@@ -2,32 +2,10 @@ import random
 import time
 
 
-def get_rank_digit_str(number, length, rank):
-	num_as_str = str(number)
-	assert len(num_as_str) >= rank
-	return int(num_as_str[rank - 1])
-
-
 def get_rank_digit(number, length, rank):
 	assert length >= rank
 	digit = (number // (10**(length - rank))) % 10
 	return digit
-
-
-def rank_sort_str(arr, length):
-	tarr = arr
-	for i in range(length, 0, -1):
-		ranks = [0] * 10
-		for j in tarr:
-			ranks[get_rank_digit_str(j, length, i)] += 1
-		for j in range(1, 10):
-			ranks[j] += ranks[j - 1]
-		res = [0] * len(tarr)
-		for j in range(len(tarr) - 1, -1, -1):
-			res[ranks[get_rank_digit_str(tarr[j], length, i)] - 1] = tarr[j]
-			ranks[get_rank_digit_str(tarr[j], length, i)] -= 1
-		tarr = res
-	return res
 
 
 def rank_sort(arr, length):
@@ -46,29 +24,55 @@ def rank_sort(arr, length):
 	return res
 
 
+def radix_sort(arr):
+	radix = 10
+	max_len = False
+	tmp, placement = -1, 1
+	while not max_len:
+		max_len = True
+		buckets = [list() for _ in range(radix)]
+		for i in arr:
+			tmp = i / placement
+			buckets[int(tmp % radix)].append(i)
+			if max_len and tmp > 0:
+				max_len = False
+		a = 0
+		for b in range(radix):
+			buck = buckets[b]
+			for i in buck:
+				arr[a] = i
+				a += 1
+		placement *= radix
+	return arr
+
+
 def quicksort(arr):
 	if len(arr) <= 1:
 		return arr
 	pivot = random.choice(arr)
-    less = [x for x in arr if x < pivot]
-    greats = [x for x in arr if x > pivot]
-    equals = [pivot] * arr.count(pivot)
-    return quicksort(less) + equals + quicksort(greats)
+	less = [x for x in arr if x < pivot]
+	greats = [x for x in arr if x > pivot]
+	equals = [pivot] * arr.count(pivot)
+	return quicksort(less) + equals + quicksort(greats)
 
 
 length = 100
-numbers = 10000
+numbers = 10
 print('Генерация массива из {} числел разрядностью {}'.format(numbers, length))
-arr = [random.randint(10   **(length-1), 10**length - 1) for _ in range(numbers)]
+arr = [random.randint(10**(length-1), 10**length - 1) for _ in range(numbers)]
+print(arr)
 start = time.time() * 1000
 sorted_arr = rank_sort(arr, length)
 end = time.time() * 1000
 print('Затрачено на поразрядную сортировку {} мс (разряд вычисляется)'.format(end - start))
+print(sorted_arr)
 start = time.time() * 1000
-sorted_arr = rank_sort_str(arr, length)
+sorted_arr = radix_sort(arr)
 end = time.time() * 1000
-print('Затрачено на поразрядную сортировку {} мс (разряд из строки)'.format(end - start))
+print('Затрачено на поразрядную сортировку {} мс (разряд вычисляется, реализация чужая)'.format(end - start))
+print(sorted_arr)
 start = time.time() * 1000
 sorted_Arr = quicksort(arr)
 end = time.time() * 1000
 print('Затрачено на быструю^3 сортировку {} мс'.format(end - start))
+print(sorted_arr)
